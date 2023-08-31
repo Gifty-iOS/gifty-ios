14a43f17b193f14192263a18ecfd1a9fbd5f212b
MojitoBar <wnehdtjr5@gmail.com>
2023-08-31 18:06:29 +0900

    권한 없을 때 alert 추가

--- Gifty/Service/PhotoLibraryService.swift
+++ Gifty/Service/PhotoLibraryService.swift
@@ -5,10 +5,33 @@
 //  Created by jito on 8/31/23.
 //
 
-import Foundation
+import UIKit
 import Photos
+import Jito
 
 class PhotoLibraryService {
+    func createPhotoLibraryRequestAlert() -> UIAlertController {
+        let alertController = UIAlertController(title: "사진 앨범 접근 권한 필요", message: "이 기능을 사용하기 위해선 사진 앨범 접근이 필요합니다.", preferredStyle: .alert)
+        
+        let allowAction = UIAlertAction(title: "설정으로", style: .default) { _ in
+            let appSettings = #unwrap(URL(string: UIApplication.openSettingsURLString), message: "appSetting 경로가 잘못 되었습니다.")
+            if !UIApplication.shared.canOpenURL(appSettings) {
+                fatalError("열수 없는 링크입니다.")
+            }
+            
+            UIApplication.shared.open(appSettings)
+        }
+        
+        let denyAction = UIAlertAction(title: "닫기", style: .cancel) { _ in
+            alertController.dismiss(animated: true)
+        }
+        
+        alertController.addAction(allowAction)
+        alertController.addAction(denyAction)
+        
+        return alertController
+    }
+    
     func requestPermission(completion: @escaping (Bool) -> Void) {
         let photosAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
         
